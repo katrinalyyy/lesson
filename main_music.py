6936208823340d1d9c53b8b0dbbce4398ecac1c2
Maximka01 <mlokotkov@ya.ru>
2022-04-14 18:20:19 +0300

    Update main_music.py

--- main_music.py
+++ main_music.py
@@ -4,11 +4,40 @@ from typing import List
 
 
 class Music:
-def __init__(self, id: int, name:str, author:str, genre: str, year: int, duration: str):
+    def __init__(self, id: int, name: str, author: str, genre: str, year: int, duration: str):
         self.id = id
         self.name = name
         self.author = author
         self.genre = genre
         self.year = year
         self.duration = duration
+
+
+class Audioteka:
+    def __init__(self):
+        self.books: List[Music] = []
+
+    def load_music(self, music: List[Music]):
+        self.music = music
+
+
+def load_data(library: Audioteka):
+    if os.path.exists('database_users.dat'):
+        with open('database_music.dat', 'rb') as f:
+            size = pickle.load(f)
+            music = []
+            for i in range(size):
+                music.append(pickle.load(f))
+            library.load_music(music)
+    else:
+        with open('database_music.dat', 'wb') as f:
+            pickle.dump(0, f)
+
+
+def save(file_name: str, data: List):
+    with open(file_name, 'wb') as f:
+        pickle.dump(len(data), f)
+        for item in data:
+            pickle.dump(item, f)
+
 
